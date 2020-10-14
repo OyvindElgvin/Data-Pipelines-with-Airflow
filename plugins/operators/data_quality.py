2da47b137ff91d4a6660de5fca2ba5acb73a6705
Oyvind_Elgvin <stoyvind@gmail.com>
2020-10-14 15:30:49 +0200

    docs: removed unnecessary comments

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,9 +8,6 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
                  redshift_conn_id="",
                  table_list="",
                  columns="",
@@ -18,9 +15,6 @@ class DataQualityOperator(BaseOperator):
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
         self.table_list = table_list
         self.columns = columns
         self.redshift_conn_id = redshift_conn_id
@@ -28,16 +22,8 @@ class DataQualityOperator(BaseOperator):
 
     def execute(self, context):
         self.log.info('Starting DataQualityOperator')
-        #aws_hook = AwsHook(self.aws_credentials_id)
-        #credentials = aws_hook.get_credentials()
         redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
-        """
-        For example one test could be a SQL statement that checks if certain column
-        contains NULL values by counting all the rows that have NULL in the column.
-        We do not want to have any NULLs so expected result would be 0 and the test
-        would compare the SQL statement's outcome to the expected result.
-        """
         for table, col in zip(self.table_list, self.columns):
 
             # check if sql statement is correct
@@ -53,7 +39,8 @@ class DataQualityOperator(BaseOperator):
                 raise ValueError(f"""\n----[TEST FAILED] - Data quality check failed.
                                                        There are {an_int_fianlly_ffs} in column {col} in table {table}""")
             else:
-                self.log.info(f"\n----[TEST PASSED] - Data quality on table {table} check passed. {table} had {an_int_fianlly_ffs} nulls")
+                self.log.info(f"""\n----[TEST PASSED] - Data quality on table {table} check passed.
+                                                        {table} had {an_int_fianlly_ffs} nulls""")
 
 
             # actual records test 1
